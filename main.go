13747f68f33530177b3a3998ee0b44a03211d97b
Josiah S <code@salix.dev>
2024-09-07 17:33:17 -0700

    attempt at deciding whether to respond and <various_tags>

--- main.go
+++ main.go
@@ -101,11 +101,11 @@ func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 	}
 
 	// check message to see if bot was mentioned
-	if !wasMentioned(s, m) || m.MentionEveryone {
-		return
-	}
+	// if !wasMentioned(s, m) || m.MentionEveryone {
+	// 	return
+	// }
 
-	// TODO set discord typing indicator
+	// TODO set discord typing indicator, or for now just tag message with emoji--not sure how yet
 
 	// store authors' usernames for stop strings later
 	usernames := make([]string, 0)
@@ -131,8 +131,8 @@ func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 		}
 	}
 
-	// also grab the last 10 messages (deduplicated by id)
-	contextMessages, err := s.ChannelMessages(m.ChannelID, 10, m.ID, "", "")
+	// also grab the last X messages (deduplicated by id)
+	contextMessages, err := s.ChannelMessages(m.ChannelID, 20, m.ID, "", "")
 	if err != nil {
 		fmt.Println("couldn't retrieve channel messages", err)
 	} else {
@@ -149,33 +149,29 @@ func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 	slices.Reverse(messages)
 
 	// build prompt w/ instructions, persona, and message history
-	systemSeq := prompt.MISTRAL_V2.SystemStart
-	systemEnd := prompt.MISTRAL_V2.SystemEnd
-	userSeq := prompt.MISTRAL_V2.UserStart
-	modelSeq := prompt.MISTRAL_V2.UserEnd + prompt.MISTRAL_V2.ModelStart
+	systemSeq := prompt.GEMMA_V2.UserStart
+	// systemEnd := prompt.GEMMA_V2.SystemEnd
+	// userSeq := prompt.GEMMA_V2.UserStart
+	modelSeq := prompt.GEMMA_V2.UserEnd + prompt.GEMMA_V2.ModelStart
 
 	// all the stop strings
-	stopStrings := []string{prompt.MISTRAL_V2.ModelEnd, "\n\n\n", "\n---", "\n(Note:"}
+	stopStrings := []string{prompt.GEMMA_V2.ModelEnd, "\n\n\n", "\n---", "\n(Note:"}
 	stopStrings = append(stopStrings, usernames...)
 
-	var prompt strings.Builder
-
-	prompt.WriteString(systemSeq + fmt.Sprintf(SYSTEM_PROMPT, PERSONA, PERSONA_DESC) + systemEnd)
-	prompt.WriteString(userSeq + "## Conversation\n")
-
+	var messagesPrompt strings.Builder
 	for _, message := range messages {
-		prompt.WriteString("\n[" + message.Timestamp.Format("2006-01-02 15:04:05") + "] " + message.Author.Username + ": " + trimSuffixes(message.ContentWithMentionsReplaced(), &stopStrings))
+		messagesPrompt.WriteString("\n<entry>\n<timestamp>" + message.Timestamp.Format("2006-01-02 15:04:05") + "</timestamp>\n<username>" + message.Author.Username + "</username>\n<contents>" + trimSuffixes(message.ContentWithMentionsReplaced(), &stopStrings) + "</contents>\n</entry>")
 	}
 
-	prompt.WriteString(modelSeq + PERSONA + ":")
+	prompt := systemSeq + fmt.Sprintf(SYSTEM_PROMPT, PERSONA, PERSONA_DESC, messagesPrompt.String()) + modelSeq
 
 	params := &api.KoboldParams{
-		MaxContextLength: 16384,
+		MaxContextLength: 8192,
 		MaxLength:        250,
 		Temperature:      0.5,
 		DynaTempRange:    0,
 		TopP:             1,
-		MinP:             0.1,
+		MinP:             0.05,
 		TopK:             0,
 		TopA:             0,
 		Typical:          1.0,
@@ -188,7 +184,7 @@ func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 		StopSequence:     stopStrings,
 		BanTokens:        false,
 		TrimStop:         true,
-		Prompt:           prompt.String(),
+		Prompt:           prompt,
 	}
 
 	response, err := Api.Generate(params)
@@ -197,7 +193,13 @@ func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 	}
 
 	if response.Status == "ok" {
-		processedResponse := trimSuffixes(response.Text, &stopStrings)
+		// check for <no_message>
+		if strings.Contains(response.Text, "<no_message>") {
+			return
+		}
+
+		// otherwise send bot message
+		processedResponse := trimSuffixes(strings.TrimSpace(strings.Replace(extractTagContents(response.Text, "<message>", "</message>"), PERSONA+": ", "", -1)), &stopStrings)
 		s.ChannelMessageSend(m.ChannelID, processedResponse)
 	}
 
@@ -206,19 +208,19 @@ func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 	// TODO generate custom mood status
 }
 
-func wasMentioned(s *discordgo.Session, m *discordgo.MessageCreate) bool {
-	mentioned := false
-	for _, mention := range m.Mentions {
-		if mention.ID == s.State.User.ID {
-			mentioned = true
-			break
-		}
-	}
-	if strings.Contains(strings.ToLower(m.Content), strings.ToLower(PERSONA)) {
-		mentioned = true
-	}
-	return mentioned
-}
+// func wasMentioned(s *discordgo.Session, m *discordgo.MessageCreate) bool {
+// 	mentioned := false
+// 	for _, mention := range m.Mentions {
+// 		if mention.ID == s.State.User.ID {
+// 			mentioned = true
+// 			break
+// 		}
+// 	}
+// 	if strings.Contains(strings.ToLower(m.Content), strings.ToLower(PERSONA)) {
+// 		mentioned = true
+// 	}
+// 	return mentioned
+// }
 
 func trimSuffixes(str string, suffixes *[]string) string {
 	for _, suffix := range *suffixes {
@@ -228,4 +230,12 @@ func trimSuffixes(str string, suffixes *[]string) string {
 		}
 	}
 	return str
+}
+
+func extractTagContents(str, startTag, endTag string) string {
+	if !strings.Contains(str, startTag) || !strings.Contains(str, endTag) {
+		return ""
+	}
+	secondHalf := strings.Split(str, startTag)[1]
+	return strings.Split(secondHalf, endTag)[0]
 }
--- prompt/sequence.go
+++ prompt/sequence.go
@@ -17,3 +17,12 @@ var MISTRAL_V2 = ModelFormat{
 	ModelStart:  "",
 	ModelEnd:    "</s>",
 }
+
+var GEMMA_V2 = ModelFormat{
+	SystemStart: "<start_of_turn>system\n",
+	SystemEnd:   "<end_of_turn>",
+	UserStart:   "<start_of_turn>user\n",
+	UserEnd:     "<end_of_turn>",
+	ModelStart:  "<start_of_turn>model\n",
+	ModelEnd:    "<end_of_turn>",
+}
